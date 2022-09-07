ad89586e3a50586190accb6f95fc9f04ea0f0c55
lukasz-rdzanek <lukasz.rdzanek@protonmail.com>
2022-09-07 02:23:10 +0200

    #nextGenApp prepare constants file

---
+++ app/src/shared/constants.ts
@@ -0,0 +1,5 @@
+export const SEARCH_API_SUBMIT_TEXT = "Search";
+export const SEARCH_API_INPUT_PLACEHOLDER = "Enter API name…";
+export const SEARCH_API_LABEL = "Search for a Public API";
+
+export const API_REQUEST_URL = "https://api.publicapis.org/entries";
