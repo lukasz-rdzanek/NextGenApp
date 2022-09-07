488d5e91daa98cbf186111dc0a1b7e128d53fc6c
lukasz-rdzanek <lukasz.rdzanek@protonmail.com>
2022-09-07 02:21:32 +0200

    #nextGenApp prepare Label component

---
+++ app/src/components/Label/Label.tsx
@@ -0,0 +1,13 @@
+
+import "./Label.css";
+
+interface ILabel {
+    label: string;
+    htmlFor: string;
+}
+
+const Label = ({ label, htmlFor }: ILabel) => {
+    return <label className="label" htmlFor={htmlFor}>{label}</label>
+}
+
+export default Label;
